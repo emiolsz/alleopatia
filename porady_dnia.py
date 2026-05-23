246e2596c5c7308e436cdad4354a57c87bca40df
Emilia Olszewska <120374833+emiolsz@users.noreply.github.com>
2026-05-23 12:30:08 +0200

    Update porady_dnia.py

--- porady_dnia.py
+++ porady_dnia.py
@@ -1,12 +1,17 @@
-porady_dnia = [
-    "🌱 Nie podlewaj w pełnym słońcu — krople mogą przypalać liście.",
-    "🍅 Pomidory lubią głębokie podlewanie, ale rzadziej.",
-    "🌿 Zioła najlepiej zbierać rano, zanim słońce odbierze aromat.",
-    "🥕 Marchew lepiej kiełkuje w stale wilgotnej glebie.",
-    "🌸 Usuwanie przekwitłych kwiatów pobudza dalsze kwitnienie.",
-    "🍎 Drzewa owocowe warto podlewać rzadziej, ale obficie.",
-    "🐌 Rozsypane skorupki jaj pomagają ograniczyć ślimaki.",
-    "🌧️ Po deszczu warto spulchnić glebę, by nie tworzyła skorupy.",
-    "🌱 Kompost działa najlepiej, gdy jest lekko wilgotny.",
-    "🪴 Nie przesadzaj z nawozem — przenawożenie szkodzi bardziej niż niedobór.",
-]
+porady_dnia = {
+    1: "Porada na 1 stycznia...",
+    143: "Porada na dziś sobota (23 maja):🍅 Pomidory lubią głębokie podlewanie, ale rzadziej.",
+    144: "Porada na dziś niedziela (24 maja):🌿 Zioła najlepiej zbierać rano, zanim słońce odbierze aromat.",
+    145: "Porada na dziś poniedziałek (25 maja):🥕 Marchew lepiej kiełkuje w stale wilgotnej glebie.",
+    146: "Porada na dziś wtorek (26 maja):🐌 Rozsypane skorupki jaj pomagają ograniczyć ślimaki.",
+    147: "Porada na dziś środa (27 maja):🌸 Usuwanie przekwitłych kwiatów pobudza dalsze kwitnienie.",
+    148: "Porada na dziś czwartek (28 maja):🌱 Nie podlewaj w pełnym słońcu — krople mogą przypalać liście.",
+    149: "Porada na dziś piątek (29 maja):🍎 Drzewa owocowe warto podlewać rzadziej, ale obficie.",
+    150: "Porada na dziś sobotę (30 maja):🌧️ Po deszczu warto spulchnić glebę, by nie tworzyła skorupy.",
+    151: "Porada na dziś sobotę (30 maja):🌱 Kompost działa najlepiej, gdy jest lekko wilgotny.",
+    152: "Porada na dziś sobotę (1 czerwca):🪴 Nie przesadzaj z nawozem — przenawożenie szkodzi bardziej niż niedobór.",
+    
+    
+    365: "Porada na 31 grudnia..."
+}
+
