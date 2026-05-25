972c455b240ad6380e7bf260e3962699d2cd13ba
Emilia Olszewska <120374833+emiolsz@users.noreply.github.com>
2026-05-25 14:14:37 +0200

    Update ziola.py

--- ziola.py
+++ ziola.py
@@ -1,50 +1,87 @@
-"Tymianek": {
+ziola_baza = {
+    "Tymianek": {
         "typ": "Zioło Wspierające", "ph": "6.5 - 7.5", "swiatlo": "Pełne słońce (Południe)", "woda": "Bardzo niskie", "gleba": "Sucha, piaszczysto-gliniasta",
         "porada": "Odstrasza ślimaki nagie oraz gąsienice bielinka. Sadź na słonecznych obrzeżach grządek z kapustą i sałatą.",
         "korzystne": ["Kapusta", "Brokuł", "Sałata", "Pomidor"], "niekorzystne": ["Majeranek"]
-},
-"Melisa lekarska": {
+    },
+    "Melisa lekarska": {
         "typ": "Zioło Wspierające", "ph": "6.0 - 7.0", "swiatlo": "Słoneczne do półcienia", "woda": "Średnie", "gleba": "Żyzna, wilgotna",
         "porada": "Intensywny cytrynowy zapach silnie wabi pszczoły, poprawiając zapylanie sąsiadujących warzyw. Sadź w narożnikach grządek pomidorów.",
         "korzystne": ["Pomidor", "Ogórek", "Drzewa owocowe"], "niekorzystne": ["Brak"]
-},
-"Bazylia": {
+    },
+    "Bazylia": {
         "typ": "Zioło Wspierające", "ph": "6.0 - 7.0", "swiatlo": "Pełne słońce, osłonięte", "woda": "Regularne (nie przelewać)", "gleba": "Żyzna, przepuszczalna, próchnicza",
         "porada": "Doskonały partner dla pomidorów – poprawia ich smak i intensywny aromat, a zapach bazylii skutecznie odstrasza mączliki oraz komary. Uszczykuj czubki, aby ładnie się krzewiła.",
         "korzystne": ["Pomidor", "Papryka", "Cukinia", "Aksamitka"], "niekorzystne": ["Szałwia", "Majeranek"]
-},
-"Pokrzywa zwyczajna": {
+    },
+    "Pokrzywa zwyczajna": {
         "typ": "Zioło Wspierające", "ph": "6.0 - 7.0", "swiatlo": "Półcień", "woda": "Średnie", "gleba": "Bardzo bogata w azot",
         "porada": "Kontrolowana kępa w ogrodzie zwiększa produkcję olejków eterycznych w ziołach i warzywach rosnących obok. Genialny surowiec na gnojówki wzmacniające.",
         "korzystne": ["Mięta", "Szałwia", "Pomidor"], "niekorzystne": ["Brak"]
-},
-"Rumianek": {
+    },
+    "Rumianek": {
         "typ": "Zioło Wspierające", "ph": "6.0 - 7.5", "swiatlo": "Słoneczne", "woda": "Niskie", "gleba": "Lekka, piaszczysta",
         "porada": "Zapobiega chorobom grzybowym (np. zgorzeli siewek). Sadź pojedyncze rośliny co kilka metrów w rzędach z cebulą lub kapustą.",
         "korzystne": ["Cebula", "Por", "Kapusta", "Ziemniak"], "niekorzystne": ["Mięta"]
-},
-"Oregano (Lebiedka pospolita)": {
-        "typ": "Zioło Wspierające", "ph": "6.5 - 7.5", "swiatlo": "Mocne słońce (Południe)", "woda": "Niskie", "gleba": "Przepuszczalna, kamienista",
+    },
+    "Oregano (Lebiedka pospolita)": {
+        "typ": "Zioło Wspierające", "ph": "6.5 - 7.5", "swialto": "Mocne słońce (Południe)", "woda": "Niskie", "gleba": "Przepuszczalna, kamienista",
         "porada": "Prawdziwy magnes na motyle i pożyteczne owady drapieżne. Chroni kapustne przed szkodnikami. Posadź na suchym, wystawionym na południe brzegu zagonu.",
         "korzystne": ["Kapusta", "Brokuł", "Papryka", "Dynia", "Por"], "niekorzystne": ["Brak"]
-},
-"Anyż": {
+    },
+    "Anyż": {
         "typ": "Zioło Wspierające", "ph": "6.0 - 7.0", "swiatlo": "Ciepłe, słoneczne", "woda": "Średnie", "gleba": "Żyzna, przepuszczalna",
         "porada": "Olejki anyżu stymulują kiełkowanie nasion innych roślin i odstraszają mszyce. Dobrze rośnie posadzony blisko kolendry.",
         "korzystne": ["Kolendra", "Kapusta"], "niekorzystne": ["Koper włoski", "Marchew"]
-},
-"Heliotrop": {
+    },
+    "Heliotrop": {
         "typ": "Zioło Wspierające", "ph": "6.0 - 6.5", "swiatlo": "Pełne słońce", "woda": "Regularne", "gleba": "Żyzna, próchnicza",
         "porada": "Pachnie intensywnie wanilią, wabiąc setki zapylaczy. Warto sadzić w donicach blisko wejścia do szklarni lub pod krzewami owocowymi.",
         "korzystne": ["Winorośl", "Morela", "Truskawki"], "niekorzystne": ["Brak"]
-},
-"Lubczyk": {
+    },
+    "Lubczyk": {
         "typ": "Zioło Wspierające", "ph": "6.0 - 7.0", "swiatlo": "Słoneczne do półcienia", "woda": "Wysokie", "gleba": "Głęboka, bogata w kompost",
         "porada": "Działa jak naturalny stymulator zdrowotności ogrodu – poprawia smak i wzrost niemal wszystkich warzyw rosnących w promieniu metra.",
         "korzystne": ["Wszystkie warzywa (zwłaszcza Sałata, Pomidor)"], "niekorzystne": ["Brak"]
-},
-"Szałwia lekarska": {
+    },
+    "Szałwia lekarska": {
         "typ": "Zioło Wspierające", "ph": "6.5 - 7.5", "swiatlo": "Mocne słońce", "woda": "Niskie", "gleba": "Dobrze zdrenowana, wapienna",
         "porada": "Skutecznie odstrasza motyla tantnisia krzyżowiaczka i ślimaki. Nigdy nie sadź jej obok ogórków, gdyż hamuje ich wzrost!",
         "korzystne": ["Marchew", "Kapusta", "Truskawka", "Rozmaryn"], "niekorzystne": ["Ogórek"]
+    },
+    "Mięta pieprzowa": {
+        "typ": "Zioło Wspierające", "ph": "6.0 - 7.0", "swiatlo": "Półcień do słonecznego", "woda": "Wysokie", "gleba": "Żyzna, próchnicza, stale wilgotna",
+        "porada": "Silnie odstrasza myszy, mrówki, mszyce oraz bielinka kapustnika. Sadź wyłącznie w barierach korzeniowych lub donicach, ponieważ jest skrajnie ekspansywna i zagłuszy sąsiadów.",
+        "korzystne": ["Kapusta", "Brokuł", "Brukselka", "Groch", "Pomidor"], "niekorzystne": ["Rumianek"]
+    },
+    "Rozmaryn lekarski": {
+        "typ": "Zioło Wspierające", "ph": "6.0 - 7.5", "swiatlo": "Pełne słońce", "woda": "Niskie", "gleba": "Przepuszczalna, piaszczysta, sucha",
+        "porada": "Jego zapach silnie dezorientuje połyśnicę marchwiankę oraz śmietkę kapuścianą. Świetny strażnik dla warzyw korzeniowych.",
+        "korzystne": ["Marchew", "Kapusta", "Fasola", "Szałwia"], "niekorzystne": ["Ogórek"]
+    },
+    "Majeranek ogrodowy": {
+        "typ": "Zioło Wspierające", "ph": "6.5 - 7.5", "swiatlo": "Pełne słońce", "woda": "Umiarkowane", "gleba": "Lekka, przepuszczalna, ciepła",
+        "porada": "Poprawia wigor i walory smakowe uprawianych obok warzyw. Bardzo dobrze wpływa na rozwój cebuli.",
+        "korzystne": ["Cebula", "Marchew", "Szparagi"], "niekorzystne": ["Tymianek", "Bazylia"]
+    },
+    "Kolendra siewna": {
+        "typ": "Zioło Wspierające", "ph": "6.0 - 7.0", "swiatlo": "Słoneczne", "woda": "Średnie", "gleba": "Żyzna, przepuszczalna",
+        "porada": "Odstrasza przędziorki oraz mszyce, a jej kwiaty przyciągają bzygowate zjadające szkodniki. Hamuje jednak zawiązywanie nasion u kopru.",
+        "korzystne": ["Anyż", "Marchew", "Cebula", "Kapusta"], "niekorzystne": ["Koper"]
+    },
+    "Koper ogrodowy": {
+        "typ": "Zioło Wspierające", "ph": "5.5 - 6.5", "swiatlo": "Słoneczne", "woda": "Średnie", "gleba": "Przepuszczalna, żyzna",
+        "porada": "Młody koper stymuluje wzrost i poprawia smak ogórków oraz kapusty. Dojrzałe baldachy wabią owady drapieżne polujące na mszyce.",
+        "korzystne": ["Ogórek", "Kapusta", "Brokuł", "Sałata", "Burak ćwikłowy"], "niekorzystne": ["Cebula", "Marchew", "Kolendra"]
+    },
+    "Ogórecznik lekarski": {
+        "typ": "Zioło Wspierające", "ph": "6.0 - 7.0", "swiatlo": "Słoneczne do półcienia", "woda": "Średnie", "gleba": "Średnio żyzna, przepuszczalna",
+        "porada": "Wzbogaca glebę w potas i poprawia ogólną odporność sąsiadujących roślin. Niebieskie kwiaty są jednym z najlepszych magnesów na zapylacze w ogrodzie.",
+        "korzystne": ["Truskawka", "Pomidor", "Dynia", "Cukinia"], "niekorzystne": ["Brak"]
+    },
+    "Estragon": {
+        "typ": "Zioło Wspierające", "ph": "6.0 - 7.5", "swiatlo": "Słoneczne", "woda": "Niskie do średniego", "gleba": "Przepuszczalna, sucha",
+        "porada": "Działa jak naturalny stymulator wzrostu dla większości warzyw. Jego specyficzny zapach jest bardzo nielubiany przez większość szkodników ssących.",
+        "korzystne": ["Wszystkie warzywa (zwłaszcza Bakłażan, Ziemniak, Pomidor)"], "niekorzystne": ["Brak"]
+    }
 }
