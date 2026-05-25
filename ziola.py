a2e767cafc5b8f2db33e603e5063b892562aa1df
Emilia Olszewska <120374833+emiolsz@users.noreply.github.com>
2026-05-25 14:12:39 +0200

    Update ziola.py

--- ziola.py
+++ ziola.py
@@ -1,53 +1,50 @@
-ziola_baza = {
-    "Tymianek": {
+"Tymianek": {
         "typ": "Zioło Wspierające", "ph": "6.5 - 7.5", "swiatlo": "Pełne słońce (Południe)", "woda": "Bardzo niskie", "gleba": "Sucha, piaszczysto-gliniasta",
         "porada": "Odstrasza ślimaki nagie oraz gąsienice bielinka. Sadź na słonecznych obrzeżach grządek z kapustą i sałatą.",
         "korzystne": ["Kapusta", "Brokuł", "Sałata", "Pomidor"], "niekorzystne": ["Majeranek"]
-    },
-    "Melisa lekarska": {
+},
+"Melisa lekarska": {
         "typ": "Zioło Wspierające", "ph": "6.0 - 7.0", "swiatlo": "Słoneczne do półcienia", "woda": "Średnie", "gleba": "Żyzna, wilgotna",
         "porada": "Intensywny cytrynowy zapach silnie wabi pszczoły, poprawiając zapylanie sąsiadujących warzyw. Sadź w narożnikach grządek pomidorów.",
         "korzystne": ["Pomidor", "Ogórek", "Drzewa owocowe"], "niekorzystne": ["Brak"]
-    },
-        "Bazylia": {
+},
+"Bazylia": {
         "typ": "Zioło Wspierające", "ph": "6.0 - 7.0", "swiatlo": "Pełne słońce, osłonięte", "woda": "Regularne (nie przelewać)", "gleba": "Żyzna, przepuszczalna, próchnicza",
         "porada": "Doskonały partner dla pomidorów – poprawia ich smak i intensywny aromat, a zapach bazylii skutecznie odstrasza mączliki oraz komary. Uszczykuj czubki, aby ładnie się krzewiła.",
-        "korzystne": ["Pomidor", "Ogórek", "Cukinia", "Aksamitka"], "niekorzystne": ["Szałwia", "Majeranek"]
-    },
-
-    "Pokrzywa zwyczajna": {
+        "korzystne": ["Pomidor", "Papryka", "Cukinia", "Aksamitka"], "niekorzystne": ["Szałwia", "Majeranek"]
+},
+"Pokrzywa zwyczajna": {
         "typ": "Zioło Wspierające", "ph": "6.0 - 7.0", "swiatlo": "Półcień", "woda": "Średnie", "gleba": "Bardzo bogata w azot",
         "porada": "Kontrolowana kępa w ogrodzie zwiększa produkcję olejków eterycznych w ziołach i warzywach rosnących obok. Genialny surowiec na gnojówki wzmacniające.",
         "korzystne": ["Mięta", "Szałwia", "Pomidor"], "niekorzystne": ["Brak"]
-    },
-    "Rumianek": {
+},
+"Rumianek": {
         "typ": "Zioło Wspierające", "ph": "6.0 - 7.5", "swiatlo": "Słoneczne", "woda": "Niskie", "gleba": "Lekka, piaszczysta",
         "porada": "Zapobiega chorobom grzybowym (np. zgorzeli siewek). Sadź pojedyncze rośliny co kilka metrów w rzędach z cebulą lub kapustą.",
         "korzystne": ["Cebula", "Por", "Kapusta", "Ziemniak"], "niekorzystne": ["Mięta"]
-    },
-    "Oregano (Lebiedka pospolita)": {
+},
+"Oregano (Lebiedka pospolita)": {
         "typ": "Zioło Wspierające", "ph": "6.5 - 7.5", "swiatlo": "Mocne słońce (Południe)", "woda": "Niskie", "gleba": "Przepuszczalna, kamienista",
         "porada": "Prawdziwy magnes na motyle i pożyteczne owady drapieżne. Chroni kapustne przed szkodnikami. Posadź na suchym, wystawionym na południe brzegu zagonu.",
-        "korzystne": ["Kapusta", "Brokuł", "Ogórek", "Dynia"], "niekorzystne": ["Brak"]
-    },
-    "Anyż": {
+        "korzystne": ["Kapusta", "Brokuł", "Papryka", "Dynia", "Por"], "niekorzystne": ["Brak"]
+},
+"Anyż": {
         "typ": "Zioło Wspierające", "ph": "6.0 - 7.0", "swiatlo": "Ciepłe, słoneczne", "woda": "Średnie", "gleba": "Żyzna, przepuszczalna",
         "porada": "Olejki anyżu stymulują kiełkowanie nasion innych roślin i odstraszają mszyce. Dobrze rośnie posadzony blisko kolendry.",
-        "korzystne": ["Kolendra", "Marchew"], "niekorzystne": ["Koper włoski"]
-    },
-    "Heliotrop": {
+        "korzystne": ["Kolendra", "Kapusta"], "niekorzystne": ["Koper włoski", "Marchew"]
+},
+"Heliotrop": {
         "typ": "Zioło Wspierające", "ph": "6.0 - 6.5", "swiatlo": "Pełne słońce", "woda": "Regularne", "gleba": "Żyzna, próchnicza",
         "porada": "Pachnie intensywnie wanilią, wabiąc setki zapylaczy. Warto sadzić w donicach blisko wejścia do szklarni lub pod krzewami owocowymi.",
         "korzystne": ["Winorośl", "Morela", "Truskawki"], "niekorzystne": ["Brak"]
-    },
-    "Lubczyk": {
+},
+"Lubczyk": {
         "typ": "Zioło Wspierające", "ph": "6.0 - 7.0", "swiatlo": "Słoneczne do półcienia", "woda": "Wysokie", "gleba": "Głęboka, bogata w kompost",
         "porada": "Działa jak naturalny stymulator zdrowotności ogrodu – poprawia smak i wzrost niemal wszystkich warzyw rosnących w promieniu metra.",
         "korzystne": ["Wszystkie warzywa (zwłaszcza Sałata, Pomidor)"], "niekorzystne": ["Brak"]
-    },
-    "Szałwia lekarska": {
+},
+"Szałwia lekarska": {
         "typ": "Zioło Wspierające", "ph": "6.5 - 7.5", "swiatlo": "Mocne słońce", "woda": "Niskie", "gleba": "Dobrze zdrenowana, wapienna",
         "porada": "Skutecznie odstrasza motyla tantnisia krzyżowiaczka i ślimaki. Nigdy nie sadź jej obok ogórków, gdyż hamuje ich wzrost!",
         "korzystne": ["Marchew", "Kapusta", "Truskawka", "Rozmaryn"], "niekorzystne": ["Ogórek"]
-    }
 }
