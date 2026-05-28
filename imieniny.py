bf7eaca5679b7dc5b2c135866388fb2cf869839a
Emilia Olszewska <120374833+emiolsz@users.noreply.github.com>
2026-05-28 10:19:27 +0200

    Add files via upload

---
+++ imieniny.py
@@ -0,0 +1,99 @@
+# imieniny.py
+
+baza_imienin = {
+    1: {
+        # STYCZEŃ - uzupełnij w wolnej chwili
+    },
+    2: {
+        # LUTY - uzupełnij w wolnej chwili
+    },
+    3: {
+        # MARZEC - uzupełnij w wolnej chwili
+    },
+    4: {
+        # KWIECIEŃ - uzupełnij w wolnej chwili
+    },
+    5: { # MAJ (Pełna lista imion)
+        1: "Anieli, Berte, Brioka, Filipa, Floryny, Gracjana, Jakuba, Józefa, Jeremiasza, Maurycego, Tamary, Mai",
+        2: "Anastazego, Anatola, Antonina, Atanazji, Atanazego, Borysa, Celestyny, Gwalberta, Walentego, Zoe, Waltera",
+        3: "Aleksandra, Alodii, Antoniny, Irmgardy, Jaropełka, Juwenalisa, Leonii, Świętochny, Marii, Marioli",
+        4: "Amalii, Antoniny, Floriana, Moniki, Michała, Grzegorza, Damiana, Pelagii, Teodory, Kasjana, Tekli, January, Leonidy, Paulina, Gotarda, Lucyli, Wespazjana, Przemysława",
+        5: "Ireny, Waldemara, Gotarda, Stanisława, Stanisławy, Ireneusza, Maksyma, Teodora, Hilarego, Piusa, Penelopy, Irydy, Tytusa",
+        6: "Jakuba, Filipa, Jana, Franciszka, Bartłomieja, Judyty, Benedykty, Juranda, Ewodii, Katarzyny, Gościmiła",
+        7: "Róży, Gizeli, Jana, Piotra, Floriana, Ludmiły, Wincentego, Wirginii, Augusta, Domicela, Ludomiry, Flawii, Bogumira, Domicjana, Radosława",
+        8: "Stanisława, Stanisławy, Michała, Piotra, Wiktora, Idy, Benedykta, Dionizego, Bonifacego, Arseniusza, Dezyderii, Achacjusza, Amata",
+        9: "Grzegorza, Bożydara, Karoliny, Katarzyny, Marii, Mikołaja, Stefana, Hiacynty, Beata, Otokara, Przebora",
+        10: "Antoniny, Beatrycze, Jana, Sylwestra, Feliksa, Samuela, Sylwestry, Antonina, Gordiana, Wiktoryny, Innocentego, Nazariusza, Łazarza, Symplicjusza, Izydora",
+        11: "Ignacego, Franciszka, Mirandy, Igi, Zuzanny, Filipa, Tadeusza, Maksyma, Alberta, Alojzego, Benedykta, Stelli, Berty, Adalberta, Mamerta, Gwalberta, Majoli",
+        12: "Dominika, Pankracego, Joanny, Jana, Teodory, Domiceli, Germana, Flawii, Imeldy, Achillesa, Nawoja, Jazona, Epifaniusza",
+        13: "Marii, Roberty, Serwacego, Magdaleny, Roberta, Andrzeja, Jana, Piotra, Gerarda, Aarona, Dobiesławy, Ciechosława",
+        14: "Macieja, Bonifacego, Michała, Marii, Wiktora, Dominiki, Jeremiego, Jeremiasza, Tiny, Idziego, Dobiesława, Koryny",
+        15: "Zofii, Nadziei, Izydora, Andrzeja, Nadii, Jana, Piotra, Pawła, Maksyma, Kasjusza, Atanazego, Dionizji, Wiktoryna, Florencjusza, Symplicjusza",
+        16: "Andrzeja, Szymona, Adama, Jędrzeja, Adaminy, Brendana, Honorata, Fidola, Jana Nepomucena, Wieńczysława",
+        17: "Sławomira, Weroniki, Andrzeja, Wiktora, Bruno, Brunona, Wilhelma, Antonii, Paschalisa",
+        18: "Aleksandry, Eryka, Feliksa, Sandry, Jana, Klaudii, Julity, Eryki, Fainy",
+        19: "Iwony, Iwo, Urbana, Piotra, Jana, Mikołaja, Bernardy, Augustyna, Kryspina, Teofila, Celestyna, Pudencjanny",
+        20: "Bernardyna, Kolumby, Wiktorii, Aleksandra, Dawid, Karola, Józefy, Teodora, Elfrydy, Anastazego, Saturniny, Rymwida, Bronimira",
+        21: "Wiktora, Tymoteusza, Jana Nepomucena, Leny, Krzysztofa, Donata, Teobalda, Synezjusza",
+        22: "Julii, Emila, Ryty, Wiesława, Heleny, Wiesławy, Jana, Piotra, Romana, Doriana, Marcjana, Wisławy, Krzesisławy",
+        23: "Iwony, Michała, Emilii, Jana, Juliana, Lucjusza, Symeona, Dezyderiusza, Eufrozyny, Dezyderego",
+        24: "Joanny, Zuzanny, Mileny, Jana, Marii, Dawid, Franciszka, Dagmary, Ludwika, Wincentego, Wanesy, Amalii, Tomiry, Oriona",
+        25: "Magdy, Grzegorza, Leona, Mariusza, Marcjanny, Dionizego, Zenobiusza, Bedy, Imisławy, Borzysława",
+        26: "Eweliny, Pauliny, Filipa, Angeliki, Emila, Teodora, Zachary, Zachariasza, Alwina, Lamberta",
+        27: "Oliwiera, Magdaleny, Jana, Małgorzaty, Juliana, Lucjana, Fryderyka, Augustyna, Izydora, Fryderyki, Radowita",
+        28: "Elwiry, Augustyna, Germana, Jaromira, Justa, Justyny, Priama, Wiktora, Wilhelma, Ignacego, Emila, Lucjusza, Balladyny",
+        29: "Magdaleny, Urszuli, Marii, Aleksandra, Bogusława, Andrzeja, Stefana, Rajmunda, Wilhelma, Maksymina",
+        30: "Ferdynanda, Joanny, Jan, Andrzeja, Feliksa, Bazylego, Żanny, Anastazego, Andronika, Zyndrama, Gawina",
+        31: "Kamili, Aniela, Petroneli, Feliksa, Marietta, Noe, Witalis, Sylwiusza"
+    },
+    6: { # CZERWIEC (Pełna lista imion)
+        1: "Jakuba, Konrada, Pameli, Justyna, Magdaleny, Pawła, Nikodema, Juliana, Bernarda, Alfonsa, Euniki, Fortunata, Feliny, Świętopełka, Teobalda, Hannibala, Angeli, Sylwestra",
+        2: "Marianny, Marzanny, Eugeniusza, Piotra, Marii, Mikołaja, Blandyny, Florianny, Marcelina, Erazma, Efrema, Sadoka, Fotyna, Nicefora, Eugenie",
+        3: "Leszka, Kewina, Karola, Piotra, Jana, Franciszka, Macieja, Andrzeja, Pauli, Tamary, Joachima, Wawrzyńca, Ferdynanda, Izaaka, Klotyldy, Owidiusza, Zdzisława",
+        4: "Karola, Franciszka, Dacjana, Saturniny, Kwiryny, Kwiryna, Pacyfika, Dominika, Franciszki",
+        5: "Walerii, Waltera, Bonifacego, Igora, Jakuba, Ferdynanda, Zenaidy, Genadiusza, Doroteusza, Walentego, Edwarda, Wiktora",
+        6: "Pauliny, Norberta, Dominiki, Aleksandra, Marii, Filipa, Gerarda, Klaudiusza, Marcelego, Wawrzyńca, Sydonii, Artemiusza, Laurentego",
+        7: "Roberta, Wiesławy, Wiesława, Piotra, Anny, Antoniego, Pawła, Jarosława, Teresy, Jeremiego, Jeremiasza, Lukrecji, Sydonii",
+        8: "Seweryna, Medarda, Jakuba, Marii, Wilhelma, Medardy, Maksymina",
+        9: "Pelagii, Felicjana, Anny, Ryszarda, Józefa, Sylwestra, Sylwestry, Bertranda, Efrema",
+        10: "Małgorzaty, Bogumiła, Bogumiły, Jana, Henryka, Diany, Tymoteusza, Cecylii, Maksyma, Edgara, Aureliusza, Onufrego, Amaty",
+        11: "Pauli, Barnaby, Anastazego, Jana, Feliksa, Teodozji, Flory, Radomiła, Radomiły, Fortunata",
+        12: "Mieczysławy, Mieczysława, Onufrego, Antoniego, Jana, Antoniny, Janiny, Władysława, Czesława, Kacpra, Leona, Kazimierza, Włodzimierza, Stefana, Zygmunta, Bolesława, Celestyny, Narcyza, Gwidona, Jarogniewa",
+        13: "Antoniego, Lucjana, Grzegorza, Olgi, Tobiasza, Gerarda, Hermana, Gracji, Akwiliny",
+        14: "Elizy, Walerego, Metodego, Michała, Ryszarda, Feliksa, Alojzego, Konstancji, Rufina, Marcjana, Anastazego, Elizeusza",
+        15: "Jolanty, Wita, Oliwii, Witolda, Bernarda, Angeliny, Adelajdy, Izoldy, Abrahama, Leonidy, Witosława, Lotara, Wisławy, Albertyny, Germany, Placydy",
+        16: "Anety, Aliny, Justyny, Benona, Jana, Judyty, Ludgardy, Aureliana, Benony",
+        17: "Laury, Adolfa, Agnieszki, Franciszka, Alberta, Marcjana, Adolfy, Radomiła, Izaury, Nikandry, Waleriany",
+        18: "Elżbiety, Marka, Amandy, Emila, Maryny, Paula, Efrema, Leoncjusza",
+        19: "Romualda, Gerwazy, Protazy, Michaliny, Julianny, Romualdy, Bonifacego, Otto, Odona, Borzysława, Gaudentego",
+        20: "Bożeny, Bogny, Florentyny, Jana, Tomasza, Rafała, Michała, Bogumiły, Franciszka, Diny, Makarego, Sylweriusza, Benigny, Hektora, Rafaeli, Baltazara, Gemmy",
+        21: "Alicji, Alojzego, Marty, Rajmunda, Rudolfa, Chloe, Euzebiusza, Alojzy, Albana, Marcji, Terencjusza",
+        22: "Tomasza, Pauliny, Paulina, Jana, Eberharda, Albana, Flawiusza, Innocentego, Achacjusza, Achacego",
+        23: "Wandy, Zenona, Jana, Piotra, Anny, Marii, Józefa, Zenona, Albina, Sydonii, Edeltruda, Agrypiny, Atanazego",
+        24: "Jana, Danuty, Janiny, Emilii, Janusza, Romualdy, Longina, Wilhelma, Longina, Janisława, Symplicjusza",
+        25: "Doroty, Łucji, Prospera, Maksyma, Wilhelma, Tolisława, Adelberta, Febronii",
+        26: "Pawła, Jana, Dawida, Andrzeja, Jeremiego, Pelagii, Jeremiasza, Edburga, Maksencjusza",
+        27: "Władysława, Marii, Jana, Joanny, Teresy, Maryli, Cyryla, Bożydara, Samson, Włodzisława, Bogudara",
+        28: "Ireneusza, Leona, Pawła, Józefa, Marcela, Lubomira, Wincentyny, Sereny, Zbrosława",
+        29: "Piotra, Pawła, Beaty, Salomei, Marcelina, Synezjusza, Emmy, Judyty",
+        30: "Emilii, Lucyny, Marcjana, Władysława, Cyryli, Ciechosława, Leonii, Ostapa"
+    },
+    7: {
+        # LIPIEC - uzupełnij w wolnej chwili
+    },
+    8: {
+        # SIERPIEŃ - uzupełnij w wolnej chwili
+    },
+    9: {
+        # WRZESIEŃ - uzupełnij w wolnej chwili
+    },
+    10: {
+        # PAŹDZIERNIK - uzupełnij w wolnej chwili
+    },
+    11: {
+        # LISTOPAD - uzupełnij w wolnej chwili
+    },
+    12: {
+        # GRUDZIEŃ - uzupełnij w wolnej chwili
+    }
+}
